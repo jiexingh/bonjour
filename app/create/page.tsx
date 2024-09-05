8530127e2ee977061bc8fb06c520e8fc1db98d78
jiexinghan <2420499179@qq.com>
2024-09-05 22:21:35 +0800

    delete: create code

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,10 +1,10 @@
-import InfoDetail from '@/components/InfoDetail'
+
 import React from 'react'
 
 const Create = () => {
   return (
     <div className="container mx-auto">
-      <InfoDetail isResponsive={true}/>
+      comming soon
     </div>
   )
 }
